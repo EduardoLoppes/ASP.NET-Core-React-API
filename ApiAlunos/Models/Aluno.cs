d76b10c649681b96946de55de2ff0a30c3d0171d
EduardoLoppes <mcfgonzalles@gmail.com>
2023-09-29 11:33:10 -0300

    :sparkles: :Conectando com o banco de dados

---
+++ ApiAlunos/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiAlunos.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? Nome { get; set; }
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public int Idade { get; set; }
+    }
+}
